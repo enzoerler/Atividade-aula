f53af7f17eca68c13effd0d5e0285d1f66ec86c9
enzoerler <165963558+enzoerler@users.noreply.github.com>
2024-04-03 19:29:06 -0300

    Add files via upload

---
+++ atividade em sala.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* TIVE MUITA DIFICULDADE, NAO CONSEGUIII!!! */
+
+int main(int argc, char *argv[]) {
+	
+	int i, x, c;
+	
+	
+	printf("DIGITE NUMERO DA BASE: ");
+	scanf("%d", &c);
+	
+	
+ 	for (i = 1; i <= c; i = i + i) {
+ 		
+ 		printf (x = 1; x <= i; x = x + x){
+ 			printf("x");
+ 	
+ 			
+		 }
+    printf("\n");
+ 	} 
+
+	
+	
+	
+	return 0;
+}
+
